cbcd2cbce4b51b9b73efd3cd9aa29454bd8fa123
yangdi <yangdi@mapbar.com>
2015-02-15 16:39:53 +0800

    make empty project

--- index.rst
+++ index.rst
@@ -1,5 +1,10 @@
-NaviCore Map Rendering Documents
-===================================================
+.. interesting intelligent hardwares documentation master file, created by
+   sphinx-quickstart on Sun Feb 15 16:37:37 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to interesting intelligent hardwares's documentation!
+=============================================================
 
 Contents:
 
--- make.bat
+++ make.bat
@@ -127,9 +127,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\MapRenderingDocuments.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\interestingintelligenthardwares.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\MapRenderingDocuments.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\interestingintelligenthardwares.ghc
 	goto end
 )
 
